804fc435d96a9ab8c452feadd458203f8eddc03c
Jonas Behmer <jonas.behmer@callistaenterprise.se>
2014-12-15 20:36:38 +0100

    Added simple form to create new blog post

---
+++ app/controllers/posts/new.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.ArrayController.extend({
+  actions: {
+    createPost: function() {
+      var title = this.get('title');
+      var meta = this.get('meta');
+      var body = this.get('body');
+
+      var post = this.store.createRecord('post', {
+        title: title,
+        meta: meta,
+        body: body
+      });
+
+      this.set('title', '');
+      this.set('meta', '');
+      this.set('body', '');
+
+      // Save the new model
+      post.save();
+    }
+  }
+});
