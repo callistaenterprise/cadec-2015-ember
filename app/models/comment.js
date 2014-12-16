ccf06aefeddd85095fde030a5a16fea567fb278e
Jonas Behmer <jonas.behmer@callistaenterprise.se>
2014-12-16 14:17:17 +0100

    Added user and comment model

---
+++ app/models/comment.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  answer: DS.attr('string'),
+  date: DS.attr('date'),
+  post: DS.belongsTo('post')
+});
--- app/models/post.js
+++ app/models/post.js
@@ -2,6 +2,6 @@ import DS from "ember-data";
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  meta: DS.attr('string'),
-  body: DS.attr('string')
+  body: DS.attr('string'),
+  user: DS.belongsTo('user')
 });
