6ed62132cb5f38dda99204dd92723b1cc9badf3f
stephenwhite <stephen.white@mac.com>
2014-12-19 09:21:30 +0100

    addition of stef penners moment, jshint exception for global moment

--- app/helpers/date-time.js
+++ app/helpers/date-time.js
@@ -1,15 +1,16 @@
 import Ember from 'ember';
+/* global moment */
 
 export function dateTime(date, format) {
   if (!date) {
     return "";
   }
-  if(!format || !(typeof format == 'string' || format instanceof String)){
+  if(!format || !(typeof format === 'string' || format instanceof String)){
     return moment(date).format('YYYY-MM-DD');
   } else {
     return moment(date).format(format);
   }
-  return input;
-};
+  return date;
+}
 
 export default Ember.Handlebars.makeBoundHelper(dateTime);
--- app/routes/posts/post/comments.js
+++ app/routes/posts/post/comments.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(params) {
+  model: function() {
     var postModel = this.modelFor('posts.post');
     //return postModel.get('comments');
     return this.store.find('comment', {post_id:postModel.get('id')});
