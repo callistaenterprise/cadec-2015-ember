80983f6b81b923b4263fef62357128edb913a470
stephenwhite <stephen.white@mac.com>
2014-12-22 10:55:56 +0100

    fiddled with routing

--- app/router.js
+++ app/router.js
@@ -1,16 +1,19 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
-  this.resource('posts', function() {
-    this.route('new');
-    this.resource('posts.post', {path:'/:post_id'}, function(){
-      this.route('comments');
+  this.resource("posts", function() {
+    this.resource("posts.post", {
+      path: ":post_id"
+    }, function() {
+      this.resource("posts.post.comments", {path:'comments'}, function() {});
     });
+
+    this.route("new");
   });
 });
 
