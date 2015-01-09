b795cd7f989a2603351ea824695de960adcf4618
sedina <sedina.oruc@callistaenterprise.se>
2015-01-09 12:42:37 +0100

    added comment model and renamed post-adapter to application

--- exercise3/app/models/post.js
+++ exercise3/app/models/post.js
@@ -2,14 +2,15 @@ import DS from 'ember-data';
 
 var Post = DS.Model.extend({
   title: DS.attr('string'),
-  body: DS.attr('string')
+  body: DS.attr('string'),
+  comments : DS.hasMany('comment', {async:true})
 });
 
 Post.reopenClass({
-  FIXTURES: [
-    {'id':'1', 'title':'Stephens Blog', 'body':'Ember Rocks!'},
-    {'id':'2', 'title':'Jonases Blog', 'body':'A host, of golden daffodils'},
-    {'id':'3', 'title':'Sedinas Blog', 'body':'I wandered lonely as a cloud'}
+  FIXTURES : [
+    {'id':'1', 'title':'Stephens Blog', 'body':'Ember Rocks!', 'comments':['1', '2']},
+    {'id':'2', 'title':'Jonases Blog', 'body':'A host, of golden daffodils', 'comments':['3', '4']},
+    {'id':'3', 'title':'Sedinas Blog', 'body':'I wandered lonely as a cloud', 'comments':['5', '6']}
   ]
 
 });
