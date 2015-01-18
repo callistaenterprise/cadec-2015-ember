a39e3f9d889f2751d97c3a0008f0d759c9b05a54
sedina <sedina.oruc@callistaenterprise.se>
2015-01-18 21:04:33 +0100

    added relationship comment-user and post-user

--- exercise3/app/models/post.js
+++ exercise3/app/models/post.js
@@ -3,14 +3,15 @@ import DS from 'ember-data';
 var Post = DS.Model.extend({
   title: DS.attr('string'),
   body: DS.attr('string'),
+  user: DS.belongsTo('user'),
   comments : DS.hasMany('comment', {async:true})
 });
 
 Post.reopenClass({
   FIXTURES : [
-    {'id':'1', 'title':'Stephens Blog', 'body':'Ember Rocks!', 'comments':['1', '2']},
-    {'id':'2', 'title':'Jonases Blog', 'body':'A host, of golden daffodils', 'comments':['3', '4']},
-    {'id':'3', 'title':'Sedinas Blog', 'body':'I wandered lonely as a cloud', 'comments':['5', '6']}
+    {'id':'1', 'title':'Stephens Blog', 'body':'Ember Rocks!', 'user':'1', 'comments':['1', '2']},
+    {'id':'2', 'title':'Jonases Blog', 'body':'A host, of golden daffodils', 'user':'2', 'comments':['3', '4']},
+    {'id':'3', 'title':'Sedinas Blog', 'body':'I wandered lonely as a cloud', 'user':'3', 'comments':['5', '6']}
   ]
 
 });
