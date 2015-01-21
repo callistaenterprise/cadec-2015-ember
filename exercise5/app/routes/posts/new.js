faf7764c326ecae4bf2fc9ea79b32a2a855e3321
sedina <sedina.oruc@callistaenterprise.se>
2015-01-21 21:04:28 +0100

    removed connection with the currentUser, excluded

--- exercise5/app/routes/posts/new.js
+++ exercise5/app/routes/posts/new.js
@@ -2,10 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.createRecord('post', {
-      title : '',
-      body: '',
-      user : this.session.get('currentUser')
-    });
+    return this.store.createRecord('post');
   }
 });
