57653b6deeacd3d949c2399ff57859bba9695945
stephenwhite <stephen.white@mac.com>
2015-01-27 14:52:53 +0100

    now with integration tests!

--- bloggs-app/tests/integration/login-test.js
+++ bloggs-app/tests/integration/login-test.js
@@ -3,7 +3,7 @@ import { test } from 'ember-qunit';
 import startApp from '../helpers/start-app';
 var App;
 
-module('An Integration test', {
+module('Login integration test', {
   setup: function() {
     App = startApp();
   },
@@ -13,9 +13,15 @@ module('An Integration test', {
 });
 
 test("Page contents", function() {
-  expect(2);
+  expect(3);
   visit('/login').then(function() {
     equal(find('.navbar').length, 1, "Page contains a navbar");
     equal(find('.form-group').length, 1, "contains a form group for logging in");
+	fillIn('#login-username', 'stephen');
+	  click('#btn-login');
+
+	  andThen(function() {
+	    equal( find('#posts-list div:first:contains(Stephens Blog)').length, 1);
+	  });
   });
 });
